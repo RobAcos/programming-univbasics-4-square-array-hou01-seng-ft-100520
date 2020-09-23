800a859002bc95f425c8ee1738e098f3c622556d
Roberto Acosta <x.rojo@hotmail.com>
2020-09-23 00:32:05 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,14 @@
 def square_array(array)
+<<<<<<< HEAD
     new_array = []
   
   array.length.times do |index|
+=======
+  numbers = ["1", "2", "3"]
+  new_array = []
+  
+  numbers.length.times do |index|
+>>>>>>> a40dc0aa3991d8a689eb44b84d0cedf2c4b4752e
     new_array.push(array[index] ** 2)
   end
   new_array
